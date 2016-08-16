443bb372a04d9dcc4b7b2ccf596c4a1e9aab9481
Shreyak kumar <kumarshreyak@gmail.com>
2016-08-17 04:10:25 +0530

    Update ImageThresholding.c

--- Week 2/ImageThresholding.c
+++ Week 2/ImageThresholding.c
@@ -5,81 +5,136 @@
 #include <time.h>
 #include <stdlib.h>
 #include <math.h>
+
 int main()
 {
-    // clock_t begin=clock();
-    /*CODE HERE */
     FILE *fp;
-    int choice,row,column,temp=0,countA=0,countB=0;
+    int choice,row,column,temp1=0,temp2=0,countA=0,countB=0,temp=0;
     long double meanA=0,meanB=0,stddevA,stddevB,di,countr;
     long long len;
     char *buff;
-   /* printf("Enter the image number for which you wish to do thresholding :\n");
-    scanf("%d",&choice);
-    switch(choice){
-        case 1:*/
-            row=198,column=200;
-            int data[row*column],count[256]={0};
-            printf("Reading the input data...\n\n");
-            clock_t begin=clock();
-            fp=fopen("1.txt","r");
-            while(fscanf(fp,"%d",(data+temp))!=EOF)
-            {
-                temp++;
-            }
-            fclose(fp);
-            clock_t end=clock();
-            double time_spent =(double)(end-begin)/1000.0;
-            printf("Data read in %lfms\n",time_spent);
-            data[temp]=0;
-            for (int i = 0; i < temp; ++i)
-            {
-                count[data[i]]++;
-            }
-        fp=fopen("result.txt","w");
-            printf("Performing iterative thresholding...\n\n");
-            begin=clock();
-            for (int i = 1; i < 256; ++i)
-            {
-                countA=0,countB=0,meanA=0,meanB=0,stddevA=0,stddevB=0;
-                for (int j = 0; j < i; ++j)             //Counting elements less than threshold and their mean
-                {
-                    countA+=count[j];
-                    meanA+=count[j]*j;
-                }
-                meanA=(double)meanA/(double)countA;
-                for (int j = 0; j < i; ++j)             //Calculating std. deviation for first cluster
-                {
-                    stddevA+=pow(j-meanA,2)/(double)countA;
-                }
-                stddevA=sqrtl(stddevA);
-                for (int j = 255; j >= i; --j)      //Counting elements more than threshold and their mean
-                {
-                    countB+=count[j];
-                    meanB+=count[j]*j;
-                }
-                meanB=(double)meanB/(double)countB;
-                for (int j = 255; j >= i; --j)          //Calculating std. deviation for second cluster
-                {
-                    stddevB+=pow(j-meanB,2)/(double)countB;
-                }
-                stddevB=sqrtl(stddevB);
-                di=abs(meanA-meanB)/sqrtl(stddevA*stddevA+stddevB*stddevB);
-                countr=(double)countA/(double)countB;
-                fprintf(fp,"%d %Lf %Lf\n",i,di,countr);
-            }
-        fclose(fp);
-            end=clock();
-            time_spent =(double)(end-begin)/1000.0;
-            printf("Thresholding done in %lfms\n",time_spent);
-            printf("result.txt is genrated\n");
-           // break;
+    row=100000,column=100000;
+    int *data[row],count[256]={0};
+    for (int i = 0; i < row; ++i)
+    {
+        data[i] = (int *)malloc(sizeof(int)*column);
+    }
 
+    printf("Reading the input data...\n\n");
+    clock_t begin=clock();
+    fp=fopen("1.txt","r");
+    while(fscanf(fp,"%d",&temp)!=EOF)
+    {
+       	data[temp1][temp2]=temp;
+       	//printf("%d ",data[temp1][temp2]);
+        temp2++;
+        if(temp2==column-1){
+            temp2=0;
+            temp1++;
+        }
+    }
+    printf("\n%d %d\n",temp1,temp2);
+    fclose(fp);
+    clock_t end = clock();
+    double time_spent = (double)(end-begin)/1000.0;
+    printf("Data read in %lfms\n",time_spent);
+    for (int i = 0; i < temp1; ++i)
+    {
+        for (int j = 0; j < temp2; ++j)
+        {
+            count[data[i][j]]++;
+        }
+    }
+    if(temp1==0)
+    {
+		    for (int j = 0; j < temp2; ++j)
+		    {
+		        count[data[0][j]]++;
+		    }
+    }
+	fp=fopen("result.dat","w");
+	fprintf(fp,"%s %s %s\n","Th","D.I","nA:nB");
+	printf("Enter your choice\n1.Histogram method\n2.Cumulative Histogram method\n");
+	scanf("%d",&choice);
+	if(choice==1){
+		printf("Performing iterative thresholding using 1st method...\n\n");
+		begin=clock();
+		for (int i = 1; i < 256; ++i)
+		{
+		    countA=0,countB=0,meanA=0,meanB=0,stddevA=0,stddevB=0;
+		    for (int j = 0; j < i; ++j)             //Counting elements less than threshold and their mean
+		    {
+		        countA+=count[j];
+		        meanA+=count[j]*j;
+		    }
+		    meanA=(double)meanA/(double)countA;
+		    for (int j = 0; j < i; ++j)             //Calculating std. deviation for first cluster
+		    {
+		        stddevA+=(double)(count[j]*pow(j-meanA,2))/(double)countA;
+		    }
+		    stddevA=sqrtl(stddevA);
+		    for (int j = 255; j >= i; --j)      //Counting elements more than threshold and their mean
+		    {
+		        countB+=count[j];
+		        meanB+=count[j]*j;
+		    }
+		    meanB=(double)meanB/(double)countB;
+		    for (int j = 255; j >= i; --j)          //Calculating std. deviation for second cluster
+		    {
+		        stddevB+=(double)(count[j]*pow(j-meanB,2))/(double)countB;
+		    }
+		    stddevB=sqrtl(stddevB);
+		    di=abs(meanA-meanB)/sqrtl(stddevA*stddevA+stddevB*stddevB);
+		    countr=(double)countA/(double)countB;
+		    fprintf(fp,"%d %Lf %Lf\n",i,di,countr);
+		}
+		fclose(fp);
+		end=clock();
+		time_spent =(double)(end-begin)/1000.0;
+		printf("Thresholding done in %lfms\n",time_spent);
+		printf("result.dat is genrated\n");
+		
+	}else{
+		printf("Performing iterative thresholding using 2nd method...\n\n");
+		for (int i = 1; i < 256; ++i)
+	    {
+	        count[i+1]+=count[i];
+	    }
+		begin=clock();
+		for (int i = 1; i < 256; ++i)
+		{
+		    countA=count[i-1],countB=count[256]-count[i-1],meanA=0,meanB=0,stddevA=0,stddevB=0;
+		    for (int j = 1; j < i; ++j)             //Counting elements less than threshold and their mean
+		    {
+		        meanA+=(count[j]-count[j-1])*j;
+		    }
+		    meanA=(double)meanA/(double)countA;
+		    for (int j = 1; j < i; ++j)             //Calculating std. deviation for first cluster
+		    {
+		        stddevA+=(double)((count[j]-count[j-1])*pow(j-meanA,2))/(double)countA;
+		    }
+		    stddevA=sqrtl(stddevA);
+		    for (int j = 255; j >= i; --j)      //Counting elements more than threshold and their mean
+		    {
+		        meanB+=(count[j]-count[j-1])*j;
+		    }
+		    meanB=(double)meanB/(double)countB;
+		    for (int j = 255; j >= i; --j)          //Calculating std. deviation for second cluster
+		    {
+		        stddevB+=(double)((count[j]-count[j-1])*pow(j-meanB,2))/(double)countB;
+		    }
+		    stddevB=sqrtl(stddevB);
+		    di=abs(meanA-meanB)/sqrtl(stddevA*stddevA+stddevB*stddevB);
+		    countr=(double)countA/(double)countB;
+		    fprintf(fp,"%d %Lf %Lf\n",i,di,countr);
+		}
+		fclose(fp);
+		end=clock();
+		time_spent =(double)(end-begin)/1000.0;
+		printf("Thresholding done in %lfms\n",time_spent);
+		printf("result.dat is genrated\n");
 
-
-    //}
-    /*CODE ENDS */
-    // clock_t end=clock();
-    // double time_spent =(double)(end-begin);// Time spent in ms
-    return 0;
+	
+	}
+    	return 0;
 }
